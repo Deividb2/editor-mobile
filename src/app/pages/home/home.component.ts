6c1769767f57a938fab1db966a66c98245dd69bb
Deividb2 <deividbarreto57@gmail.com>
2025-01-08 10:18:49 -0300

    componente about

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,11 +7,12 @@ import { CardsComponent } from "../../components/cards/cards.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 import { ContentComponent } from "../../components/content/content.component";
 import { WhatsComponent } from "../../components/whats/whats.component";
+import { AboutComponent } from "../../components/about/about.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent, RangeComponent, CardsComponent, FooterComponent, ContentComponent, WhatsComponent],
+  imports: [BannerComponent, RangeComponent, CardsComponent, FooterComponent, ContentComponent, WhatsComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
