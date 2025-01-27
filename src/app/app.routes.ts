d90f56bf6dff0301f40aa916ee335de69fbc54c2
Deividb2 <deividbarreto57@gmail.com>
2025-01-26 22:06:32 -0300

    new commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { TermsComponent } from './pages/terms/terms.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Editor Mobile'
+    },
+    {
+        path: 'terms',
+        component: TermsComponent,
+        title: 'Termos e condições'
+    },
+    {
+        path: '**',
+        component: ErrorComponent,
+        title: 'Página de Erro'
+    }
+];
